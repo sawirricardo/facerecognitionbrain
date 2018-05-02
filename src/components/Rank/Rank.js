7c10f8c9e5e15c266b300b518aad2cc599297a91
sawirricardo <sawir.ricardo@gmail.com>
2018-05-03 00:03:15 +0700

    initial commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Rank = () => {
+    return <div>
+        <div className="white f3">{"Ricardo, your current rank is..."}</div>
+        <div className="white f1">{"#5"}</div>
+      </div>;
+};
+
+export default Rank;
