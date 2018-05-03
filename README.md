36b65897a0ab8c37e91235dd5e8277978b0a1302
sawirricardo <sawir.ricardo@gmail.com>
2018-05-03 15:40:10 +0700

    updated with sign in, register, signout

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-#Face Recognition Brain
-###Project by Zero-to-Mastery
+# Face Recognition Brain
+### Big Project by Zero-to-Mastery, a Udemy Course
+
+To use it, you have to:
+1. Clone this Repo
+2. Run `npm install`
+3. Run `npm start`
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
+import Register from "./components/Register/Register";
 import './App.css';
 
 const app = new Clarifai.App({
@@ -31,7 +32,9 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box:{},
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }  
 
@@ -67,16 +70,37 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+onRouteChange = (route) => {
+  if (route === 'signout') {
+    this.setState({isSignedIn: false});
+  } else if (route === 'home') {
+    this.setState({isSignedIn: true});
+  }
+  this.setState({route: route});
+}
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles' params={particlesOptions} />
-        <Navigation />
-        <SignIn />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit ={this.onButtonSubmit}/>
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+
+        {route === 'home' ?
+        <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit ={this.onButtonSubmit}/>
+          <FaceRecognition box={box} imageUrl={imageUrl}/>
+        </div>
+        : 
+        ( route === 'signin' ?
+          <SignIn onRouteChange = {this.onRouteChange}/>
+          :
+          <Register onRouteChange = {this.onRouteChange}/>
+        )
+        
+        }
       </div>
     );
   }
